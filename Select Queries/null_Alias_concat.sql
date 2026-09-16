912ba1e12404bfb5ea0fe6737aae3e5c8ef7a823
Venkat AVINASH Karra <avkarra@gmail.com>
2026-09-16 14:50:41 +0530

    null, Alias, Concat

---
+++ Select Queries/null_Alias_concat.sql
@@ -0,0 +1,16 @@
+SELECT * FROM movies;
+
+-- Retrun  top 3 movies with the highest International Takings
+SELECT * FROM movie_revenues
+WHERE international_takings IS NOT NULL
+ORDER BY international_takings DESC
+-- LIMIT 3;
+FETCH FIRST 3 ROW ONLY;
+
+-- Concat first and last names of directors, seperated by space and call this new column full_name
+SELECT CONCAT_WS(' ', first_name, last_name) AS full_name FROM directors;
+
+-- Return the actors with missing first_name or missing date_of_births
+SELECT * FROM actors
+WHERE fname IS NULL 
+OR dob IS NULL;
