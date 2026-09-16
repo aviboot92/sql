72f47c1efc2b1349e641f5175489896940bb8b7c
Venkat AVINASH Karra <avkarra@gmail.com>
2026-09-16 14:58:07 +0530

    order, limit offset

---
+++ Select Queries/order_limit_offset.sql
@@ -0,0 +1,20 @@
+-- Select American directors from oldest to youngest
+SELECT * FROM directors
+ORDER BY dob;
+
+-- Retrun distinct nationalities from the directors table
+SELECT DISTINCT nationality FROM directors;
+
+-- Return first_name, last_names and dob of the 10 youngest female actors
+SELECT fname, lname, dob FROM actors
+WHERE gender = 'F'
+ORDER BY dob DESC LIMIT 10;
+
+-- For Fetch, Offset should come before
+SELECT fname, lname, dob FROM actors
+WHERE gender = 'F'
+ORDER BY dob DESC
+OFFSET 20
+FETCH FIRST 10 ROW ONLY;
+
+SELECT * from actors;
